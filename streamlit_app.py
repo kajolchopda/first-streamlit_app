d589824444540f21723eeba25de81cfccb85f583
Kajol Chopda <70687958+kajolchopda@users.noreply.github.com>
2022-11-18 13:56:25 +0530

    Update streamlit_app.py
    Copied the code from the course

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 import streamlit
 
-streamlit.title('Healthy Diner Menu')
 streamlit.header('Breakfast Menu')
-streamlit.text('Bread Jam')
-streamlit.text('Pancakes')
-streamlit.text('Cutlet')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
